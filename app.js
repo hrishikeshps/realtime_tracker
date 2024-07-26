d72d79c1bcbde6e8b6f2f72f5bed2b58f20d13fb
Hrishikesh <hrishikeshps@gmail.com>
2024-07-26 22:43:56 +0530

    final code commit

--- app.js
+++ app.js
@@ -10,11 +10,18 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", function(socket){
+io.on("connection", function (socket) {
     console.log("Connected");
+    socket.on("send-location", function (data) {
+        io.emit("receive-location", { id: socket.id, ...data });
+    })
+
+    socket.on("disconnect", function(){
+        io.emit("user-disconnect", socket.id);
+    })
 })
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("index");
 })
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,40 @@
 const socket = io();
 
-if(navigator.geolocation){
+if (navigator.geolocation) {
     navigator.geolocation.watchPosition((position) => {
-        const {latitude, longitude} = position.coords;
-        socket.emit("send-location", {latitude, longitude});
+        const { latitude, longitude } = position.coords;
+        socket.emit("send-location", { latitude, longitude });
     }, (error) => {
         console.error('errored', error);
-    },{
+    }, {
         enableHighAccuracy: true,
         maximumAge: 0,
         timeout: 5000
     })
 }
 
-const map = L.map("map").setView([0, 0], 10)
+const map = L.map("map").setView([0, 0], 16)
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "Hrishikesh Sharma"
 }).addTo(map)
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    map.setView([latitude, longitude]);
+
+    if(markers[id]){
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+})
+
+socket.on("user-disconnect", (id) => {
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+})
